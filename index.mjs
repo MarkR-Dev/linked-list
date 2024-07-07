9c7139340451c25706cb8ff35183b3825150e27e
Mark Rodgers <markrodgers.dev@gmail.com>
2024-07-07 16:58:59 +0100

    Add console log testing of linked list methods

--- index.mjs
+++ index.mjs
@@ -1,5 +1,60 @@
 import LinkedList from "./linked-list.mjs";
 
 const list = new LinkedList();
+// list.append(1);
+// list.append(2);
+// list.log();
 
-// Todo: write some test cases and examples
+// list.prepend(0);
+// list.prepend(-1);
+// list.log();
+
+// console.log(list.size());
+// list.append(1);
+// console.log(list.size());
+
+// list.append(1);
+// list.append(2);
+// console.log(list.getHead());
+
+// list.append(1);
+// list.append(2);
+// console.log(list.getTail());
+
+// console.log(list.at(0));
+// console.log(list.at(-23));
+// list.append(1);
+// list.append(2);
+// list.append(3);
+// console.log(list.at(0));
+// console.log(list.at(1));
+// console.log(list.at(2));
+
+// list.pop();
+// list.append(1);
+// list.log();
+// list.pop();
+// list.log();
+
+// console.log(list.contains(123));
+// list.append(1);
+// console.log(list.contains(1));
+
+// console.log(list.find(2));
+// list.append(1);
+// list.append(2);
+// console.log(list.find(2));
+
+// console.log(list.toString());
+// list.append(1);
+// console.log(list.toString());
+
+// list.append(0);
+// list.insertAt(1, 1);
+// list.insertAt(2, 34);
+// list.insertAt(-1, 0);
+// list.log();
+
+// list.append(1);
+// list.removeAt(0);
+// list.log();
